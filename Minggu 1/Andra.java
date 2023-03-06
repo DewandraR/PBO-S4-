dea1f997d4b3dfd52eedea632cdd7b8662abec9c
DewandraR <91871312+DewandraR@users.noreply.github.com>
2023-03-06 15:15:15 +0700

    Add files via upload
    Tugas Minggu 1

---
+++ Minggu 1/Andra.java
@@ -0,0 +1,11 @@
+public class Andra {
+    public static void main(String[] args) {
+        String nama = "Muhammad Dewandra Rhamadan";
+        String jurusan = "Teknik Informatika";
+        String universitas = "Universitas Dian Nuswantoro";
+
+        System.out.println("Halo, nama saya adalah " + nama  );
+        System.out.println("saya adalah mahasiswa " + jurusan + " " + universitas + ".");
+        System.out.println("Saya sedang menulis program Java.");
+    }
+}
